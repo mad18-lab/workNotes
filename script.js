f600e689b7e769c842f5c75d9f7c5716f747fe64
Madhav <guptamadhav69@gmail.com>
2024-08-07 04:27:47 +0530

    commit changes

--- script.js
+++ script.js
@@ -8,6 +8,13 @@ const noteInput = function() {
 const notelist = document.getElementById("noteList");
 const form = document.getElementById('inputForm');
 
+const onDivClick = function(id) {
+    console.log(`task ${id} was clicked`);
+
+    const para = document.getElementById(id);
+    para.classList.add("line-through");
+}
+
 form.addEventListener("submit", (e) => {
     e.preventDefault();
 
@@ -16,12 +23,28 @@ form.addEventListener("submit", (e) => {
 
     const notetext = document.getElementById("noteText");
 
+    const newDiv = document.createElement('div');
+    newDiv.classList.add("mx-auto");
+    newDiv.classList.add("justify-center");
+    newDiv.classList.add("align-center");
+    newDiv.classList.add("w-[450px]");
+    newDiv.classList.add("mt-4");
+    newDiv.classList.add("mb-4");
+    newDiv.classList.add("cursor-pointer");
+
     const newEntry = document.createElement('p');
 
     newEntry.textContent = notetext.value;
     newEntry.classList.add("text-white");
     newEntry.classList.add("text-center");
     newEntry.classList.add("text-3xl");
+    newEntry.id = Math.floor(Math.random() * 100);
+    newDiv.addEventListener('click', function() {
+        onDivClick(newEntry.id);
+    });
 
-    notelist.appendChild(newEntry);
+    newDiv.appendChild(newEntry);
+    notelist.appendChild(newDiv);
+
+    notetext.value = "";
 })

607201634974bb4ef818e3c5def59dd0f07e5b10
Madhav <guptamadhav69@gmail.com>
2024-08-08 00:56:18 +0530

    commit changes

--- script.js
+++ script.js
@@ -15,6 +15,11 @@ const onDivClick = function(id) {
     para.classList.add("line-through");
 }
 
+const onImgClick = function(divId) {
+    const element = document.getElementById(divId);
+    element.remove();
+}
+
 form.addEventListener("submit", (e) => {
     e.preventDefault();
 
@@ -23,28 +28,35 @@ form.addEventListener("submit", (e) => {
 
     const notetext = document.getElementById("noteText");
 
+    const mainDiv = document.createElement('div');
+    mainDiv.classList.add("mx-auto", "relative", "justify-center", "align-center", "w-[650px]", "mt-4", "mb-4");
+    mainDiv.id = Math.floor(Math.random() * 100);
+
     const newDiv = document.createElement('div');
-    newDiv.classList.add("mx-auto");
-    newDiv.classList.add("justify-center");
-    newDiv.classList.add("align-center");
-    newDiv.classList.add("w-[450px]");
-    newDiv.classList.add("mt-4");
-    newDiv.classList.add("mb-4");
-    newDiv.classList.add("cursor-pointer");
+    newDiv.classList.add("mx-auto", "justify-center", "align-center", "w-[450px]", "mt-4", "mb-4");
+
+    mainDiv.appendChild(newDiv);
+
+    const newImg = document.createElement('img');
+    newImg.src = './trash.png';
+    newImg.classList.add("absolute", "bottom-0", "right-0", "w-5", "h-5");
+    newImg.addEventListener("click", function() {
+        onImgClick(mainDiv.id);
+    })
+
+    mainDiv.appendChild(newImg);
 
     const newEntry = document.createElement('p');
 
     newEntry.textContent = notetext.value;
-    newEntry.classList.add("text-white");
-    newEntry.classList.add("text-center");
-    newEntry.classList.add("text-3xl");
+    newEntry.classList.add("text-white", "text-center", "text-3xl", "cursor-pointer");
     newEntry.id = Math.floor(Math.random() * 100);
-    newDiv.addEventListener('click', function() {
+    newEntry.addEventListener('click', function() {
         onDivClick(newEntry.id);
     });
 
     newDiv.appendChild(newEntry);
-    notelist.appendChild(newDiv);
+    notelist.appendChild(mainDiv);
 
     notetext.value = "";
 })
